ad54e683ce8dcfd88773cd7ef281c0d4de1cb6fc
shethdhvani <sheth.dhv@husky.neu.edu>
2016-11-09 18:41:29 -0500

    test cases

--- testspec/test.js
+++ testspec/test.js
@@ -7,36 +7,102 @@ var chaiHttp = require('chai-http');
 var server = require('../server');
 // var server = require('../project/app');
 var should = chai.should();
+// var expect = require('chai').expect;
 var expect = chai.expect();
 
 var user = require('../project/model/user/user.schema.server');
 
 var express = require('express');
 
-
-
 var app = express();
 exports.app = app;
 chai.use(chaiHttp);
 
 
-describe('users', function() {
-    it('should list ALL blobs on /blobs GET', function (done) {
+describe('users service', function() {
+    it('should get all users', function (done) {
         chai.request(server)
             .get('/api/findallusers')
             .end(function(err, res){
                 res.should.have.status(200);
                 done();
-            });
-        
+            })
+    });
+
+    // it('get current user with both username and password', function (done) {
+    //     chai.request(server)
+    //         .get('/api/user')
+    //         .end(function (err, res) {
+    //             // console.log("")
+    //             // expect(req.query.username).to.not.be.null;
+    //             // expect(req.query.password).to.not.be.null;
+    //             //res.header['location'].should.include('/api/findallusers')
+    //                 expect('Location', '/api/findallusers');
+    //             done();
+    //         })
+    // });
+
+
+    it('user should be able to login', function (done) {
+        chai.request(server)
+            .get('/api/loggedIn')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            })
     });
 
 
 
-    it('should list a SINGLE blob on /blob/<id> GET');
-    it('should add a SINGLE blob on /blobs POST');
-    it('should update a SINGLE blob on /blob/<id> PUT');
-    it('should delete a SINGLE blob on /blob/<id> DELETE');
+
+    it('should be able to find user by id', function (done) {
+
+        var newUser = new user({
+            // username: 'dsheth',
+            // password: 'dsheth'
+            _id: '581e9bb02a3e503ec4537046'
+        });
+        newUser.save(function(err, data) {
+            chai.request(server)
+                .get('/api/user/'+data)
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('_id');
+                    res.body._id.should.equal(newUser._id);
+                    done();
+                })
+        });
+
+    });
+
+    it('should be able to get user', function (done) {
+
+        var newUser = new user({
+            username: 'dsheth',
+            password: 'dsheth'
+        });
+        newUser.save(function (err, data) {
+            chai.request(server)
+                .get('/api/user/'+data.id)
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('_id');
+                    res.body.should.have.property('username');
+                    res.body.should.have.property('password');
+                    res.body.username.should.equal('dsheth');
+                    res.body.password.should.equal('dsheth');
+                    res.body._id.should.equal(data.id);
+                    done();
+                })
+        });
+
+    });
+
+
 });
 
 
---
+++ testspec/testkarma.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Dhvani on 11/9/2016.
+ */
